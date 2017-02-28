fcd45fc5f3e5fe027c7bf661a4f645482cbf40a6
SamGraber <authorsamgraber@gmail.com>
2017-02-28 16:35:44 -0500

    Use redux-devtools by hand
    I was reading up on the redux dev tools and the really cool part about them is that they aren't redux specific. All you have to do is send 'states' to the dev tools and they'll be visible to be manipulated via the tool. We can then 'subscribe' to the dev tools connection for messages and easily implement the ability to 'time travel' to a specific state.

---
+++ index.js
@@ -0,0 +1,37 @@
+
+var withDevTools = (
+  // process.env.NODE_ENV === 'development' &&
+  typeof window !== 'undefined' && window.devToolsExtension
+);
+
+let devTools;
+let unsubscribe;
+let state = { counter: 0 };
+renderState();
+
+if (withDevTools) {
+	devTools = window.devToolsExtension.connect();
+	unsubscribe = devTools.subscribe((message) => {
+		// Implement monitors actions.
+		// For example time traveling:
+		if (message.type === 'DISPATCH' && message.payload.type === 'JUMP_TO_ACTION') {
+			// this.setState(message.state);
+			state = JSON.parse(message.state);
+			renderState();
+			console.log(message);
+		}
+	});
+}
+
+document.getElementById('addButton').addEventListener('click', function() {
+	state = { counter: state.counter + 1 };
+	renderState();
+	if (withDevTools) {
+		devTools.send('add', state);
+	}
+});
+
+function renderState() {
+	document.getElementById('display').innerHTML = 'Counter: ' + state.counter;
+}
+
